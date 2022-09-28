7405b8627102a123bb7403889769b3bf6a5b4b9f
7 <x>
2022-09-28 20:11:06 +0800

    fix: 更新ts

--- src/utils/native-props.ts
+++ src/utils/native-props.ts
@@ -1,16 +1,18 @@
-import React, { AriaAttributes } from 'react';
+import React from 'react';
 import type { CSSProperties, ReactElement } from 'react';
 import classNames from 'classnames';
+import { ViewProps } from '@tarojs/components';
 
 export type NativeProps<S extends string = never> = {
   className?: string;
   style?: CSSProperties & Partial<Record<S, string>>;
-} & AriaAttributes;
+} & Omit<ViewProps, 'style'>;
 
 export function withNativeProps<P extends NativeProps>(props: P, element: ReactElement) {
   const p = {
     ...element.props,
   };
+
   if (props.className) {
     p.className = classNames(element.props.className, props.className);
   }
