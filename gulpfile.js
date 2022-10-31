ea3a98ab1facca85cc301a437ad77c224174bea3
7 <x>
2022-10-31 15:13:19 +0800

    feat: 优化iconfont默认大小

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ const del = require('del');
 const ts = require('gulp-typescript');
 const tsconfig = require('./tsconfig.json');
 const rename = require('gulp-rename');
+const pxMultiplePlugin = require('postcss-px-multiple')({ times: 2 });
+const replace = require('gulp-replace');
+const postcss = require('gulp-postcss');
 
 function clean() {
   return del('./lib/**');
@@ -65,6 +68,29 @@ function buildGhPage() {
     .pipe(gulp.dest('./dist/gh'));
 }
 
+function build2xCSS() {
+  return (
+    gulp
+      .src('./lib/es/styles/iconfont.css')
+      // Hack fix since postcss-px-multiple ignores the `@supports` block
+      .pipe(replace('@supports not (color: var(--adm-color-text))', '@media screen and (min-width: 999999px)'))
+      .pipe(postcss([pxMultiplePlugin]))
+      .pipe(replace('@media screen and (min-width: 999999px)', '@supports not (color: var(--adm-color-text))'))
+      .pipe(
+        gulp.dest('./lib/es/styles', {
+          overwrite: true,
+        })
+      )
+  );
+}
+
 exports.buildGh = gulp.series(cleanGh, buildGhPage);
 
-exports.buildIcons = gulp.series(clean, buildES, gulp.parallel(buildDeclaration), copyIconfontCss, copyMetaFiles);
+exports.buildIcons = gulp.series(
+  clean,
+  buildES,
+  gulp.parallel(buildDeclaration),
+  copyIconfontCss,
+  build2xCSS,
+  copyMetaFiles
+);
